f7657ae59eb56eb3003b155fcee16016dc83779f
Øyvind Raddum Berg <elacin@gmail.com>
2016-02-28 22:22:11 +0100

    build updates, dependency bumps

--- project/Build.scala
+++ project/Build.scala
@@ -1,27 +1,37 @@
+import com.typesafe.sbt.pgp.PgpKeys
+import com.typesafe.sbt.pgp.PgpKeys._
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbt.Keys._
 import sbt._
-
+import sbtrelease.ReleasePlugin.autoImport._
 import scala.xml.Group
 
 object Build extends sbt.Build {
-  val buildSettings = Defaults.coreDefaultSettings ++ Seq(
-    organization         := "com.olvind",
-    crossScalaVersions   := Seq("2.10.5", "2.11.7"),
-    scalaVersion         := "2.11.7",
-    homepage             := Some(url("http://github.com/elacin/stringifiers")),
-    publishTo           <<= isSnapshot {
-      case true  ⇒ Some("Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
-      case false ⇒ Some("Sonatype Nexus Staging"   at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
-    },
-    pomIncludeRepository    := { x => false },
-    publishArtifact in Test := false
+  type PE = Project => Project
+
+  override val settings = super.settings ++ Seq(
+    organization       := "com.olvind",
+    scalaVersion       := "2.11.7",
+    crossScalaVersions := Seq("2.11.7", "2.10.6"),
+    homepage           := Some(url("http://github.com/oyvindberg/stringifiers"))
   )
+
+  val doPublish: PE =
+    _.settings(
+      releaseCrossBuild             := true,
+      releasePublishArtifactsAction := PgpKeys.publishSigned.value,
+      pomIncludeRepository          := { x => false },
+      publishArtifact in Test       := false,
+      publishTo                    <<= isSnapshot {
+        case true  ⇒ Some("Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
+        case false ⇒ Some("Sonatype Nexus Staging"   at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+      }
+    )
 
   val manifestSetting = packageOptions <+= (name, version, organization) map {
     (title, version, vendor) =>
       Package.ManifestAttributes(
-        "Created-By" -> "Simple Build Tool",
+        "Created-By" -> "\"Simple\" Build Tool",
         "Built-By" -> System.getProperty("user.name"),
         "Build-Jdk" -> System.getProperty("java.version"),
         "Specification-Title" -> title,
@@ -36,14 +46,14 @@ object Build extends sbt.Build {
 
   // Things we care about primarily because Maven Central demands them
   val mavenCentralFrouFrou = Seq(
-    homepage := Some(new URL("http://github.com/elacin/stringifiers")),
-    startYear := Some(2015),
+    homepage := Some(new URL("http://github.com/oyvindberg/stringifiers")),
+    startYear := Some(2016),
     licenses := Seq(("Apache 2", new URL("http://www.apache.org/licenses/LICENSE-2.0.txt"))),
     pomExtra <<= (pomExtra, name, description) {(pom, name, desc) => pom ++ Group(
       <scm>
-        <url>https://github.com/elacin/stringifiers</url>
-        <connection>scm:git:git://github.com/elacin/stringifiers.git</connection>
-        <developerConnection>scm:git:git@github.com:elacin/stringifiers.git</developerConnection>
+        <url>https://github.com/oyvindberg/stringifiers</url>
+        <connection>scm:git:git://github.com/oyvindberg/stringifiers.git</connection>
+        <developerConnection>scm:git:git@github.com:oyvindberg/stringifiers.git</developerConnection>
       </scm>
       <developers>
         <developer>
@@ -58,28 +68,43 @@ object Build extends sbt.Build {
   val sourceMapTransform = (isSnapshot, version) map {
     case (true, v) ⇒
       val a = new java.io.File("").toURI.toString.replaceFirst("/$", "")
-      val g = s"https://raw.githubusercontent.com/elacin/stringifiers/$v"
+      val g = s"https://raw.githubusercontent.com/oyvindberg/stringifiers/$v"
       s"-P:scalajs:mapSourceURI:$a->$g/"
     case (false, _) ⇒ ""
   }
 
+  val preventPublication: PE =
+    _.settings(
+      publishTo := Some(Resolver.file("Unused transient repository", target.value / "fakepublish")),
+      publishArtifact := false,
+      publishLocalSigned := (),       // doesn't work
+      publishSigned := (),            // doesn't work
+      packagedArtifacts := Map.empty)
+
+  val hasNoTests: PE =
+    _.settings(
+      sbt.Keys.test in Test := (),
+      testOnly      in Test := (),
+      testQuick     in Test := ()
+    )
+
   val sharedDeps = Def.setting(Seq(
-    "org.scalatest" %%% "scalatest" % "3.0.0-M7" % Test
+    "org.scalatest" %%% "scalatest" % "3.0.0-M15" % Test
   ))
 
   val stringifiers = crossProject.in(file("stringifiers"))
-    .settings(buildSettings ++ mavenCentralFrouFrou :_*)
+    .settings(mavenCentralFrouFrou :_*)
     .settings(
       name                 := "stringifiers",
       description          := "A standardized way of moving types to and from strings",
       libraryDependencies ++= sharedDeps.value,
       manifestSetting
     ).jsSettings(
-    scalacOptions       <+= sourceMapTransform
+      scalacOptions       <+= sourceMapTransform
   )
 
   val jvm = stringifiers.jvm
   val js  = stringifiers.js
 
-  val root = project.aggregate(jvm, js).settings(publish := {}, publishLocal := {})
+  val root = project.aggregate(jvm, js).configure(hasNoTests, preventPublication)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,3 @@
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
-
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.5")
+addSbtPlugin("org.xerial.sbt"    % "sbt-sonatype" % "1.1")
+addSbtPlugin("com.github.gseitz" % "sbt-release"  % "1.0.2")
+addSbtPlugin("org.scala-js"      % "sbt-scalajs"  % "0.6.7")
